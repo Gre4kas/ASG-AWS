a9fb61ef937e707fdbf4c8f3e0b1e0796f0e8d07
Gre4kas <misaa3818@gmail.com>
2024-08-20 22:48:36 +0300

    fix code and added .dockerignore

--- app/app.py
+++ app/app.py
@@ -1,19 +1,15 @@
 from flask import Flask
 import os
 import psycopg2
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 app = Flask(__name__)
 
 def connect_db():
     conn = psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        database=os.getenv("DB_NAME", "mydatabase"),
-        user=os.getenv("DB_USER", "user"),
-        password=os.getenv("DB_PASSWORD", "password")
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
     return conn
 
@@ -28,4 +24,4 @@ def hello_world():
     return f"Hello, World! Connected to the database version: {db_version}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv("WEB_PORT", 5000)))
+    app.run(host='0.0.0.0', port=5000)
