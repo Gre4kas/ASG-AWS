6108718b62be781e715b30db5cb44993ee4dd943
Gre4kas <misaa3818@gmail.com>
2024-08-21 16:45:43 +0300

    Added al.tf

--- terraform/database.tf
+++ terraform/database.tf
@@ -13,6 +13,7 @@ resource "aws_db_instance" "postgres" {
   storage_encrypted      = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.main.name
+  copy_tags_to_snapshot     = true
 
   tags = {
     Name = "my-postgres-db"
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -49,31 +49,3 @@ resource "aws_autoscaling_group" "app_asg" {
   }
 }
 
-# Пример использования Load Balancer (опционально)
-resource "aws_lb" "app_lb" {
-  name               = "app-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.ec2_sg.id]
-  subnets            = module.vpc.public_subnets
-
-  enable_deletion_protection = false
-}
-
-resource "aws_lb_target_group" "app_tg" {
-  name     = "app-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
-}
-
-resource "aws_lb_listener" "app_listener" {
-  load_balancer_arn = aws_lb.app_lb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
