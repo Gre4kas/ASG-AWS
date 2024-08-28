2387d3654ac497a21dc4162c50abc24a60875abe
Gre4kas <misaa3818@gmail.com>
2024-08-28 12:57:49 +0300

    database.tf

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,32 +1,32 @@
-# # Создание RDS PostgreSQL
-# resource "aws_db_instance" "postgres" {
-#   allocated_storage            = 10
-#   db_name                      = "mydatabase"
-#   engine                       = "postgres"
-#   engine_version               = "16.4"
-#   instance_class               = "db.t3.micro"
-#   username                     = "foo"
-#   password                     = "foobarbaz"
-#   parameter_group_name         = "default.postgres16"
-#   skip_final_snapshot          = true
-#   publicly_accessible          = false # for prod need false
-#   storage_encrypted            = true
-#   vpc_security_group_ids       = [aws_security_group.rds_sg.id]
-#   db_subnet_group_name         = aws_db_subnet_group.main.name
-#   copy_tags_to_snapshot        = true
-#   multi_az                     = true
-#   performance_insights_enabled = true
+# Создание RDS PostgreSQL
+resource "aws_db_instance" "postgres" {
+  allocated_storage            = 10
+  db_name                      = "mydatabase"
+  engine                       = "postgres"
+  engine_version               = "16.4"
+  instance_class               = "db.t3.micro"
+  username                     = "foo"
+  password                     = "foobarbaz"
+  parameter_group_name         = "default.postgres16"
+  skip_final_snapshot          = true
+  publicly_accessible          = false # for prod need false
+  storage_encrypted            = true
+  vpc_security_group_ids       = [aws_security_group.rds_sg.id]
+  db_subnet_group_name         = aws_db_subnet_group.main.name
+  copy_tags_to_snapshot        = true
+  multi_az                     = true
+  performance_insights_enabled = true
   
-#   tags = {
-#     Name = "my-postgres-db"
-#   }
-# }
+  tags = {
+    Name = "my-postgres-db"
+  }
+}
 
-# # Создание DB Subnet Group для RDS
-# resource "aws_db_subnet_group" "main" {
-#   name       = "main-subnet-group"
-#   subnet_ids = module.vpc.private_subnets
-#   tags = {
-#     Name = "main-db-subnet-group"
-#   }
-# }
+# Создание DB Subnet Group для RDS
+resource "aws_db_subnet_group" "main" {
+  name       = "main-subnet-group"
+  subnet_ids = module.vpc.private_subnets
+  tags = {
+    Name = "main-db-subnet-group"
+  }
+}
